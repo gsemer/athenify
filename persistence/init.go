d60a663e6f3fd0e546b8f19a959166b293355134
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-07-30 18:29:37 +0300

    init db

---
+++ persistence/init.go
@@ -0,0 +1,23 @@
+package persistence
+
+import (
+	"athenify/domain"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB(username, password, ip, port, name string) (*gorm.DB, error) {
+	source := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, ip, port, name)
+	db, err := gorm.Open(postgres.Open(source), &gorm.Config{})
+	if err != nil {
+		return &gorm.DB{}, err
+	}
+
+	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+	db.AutoMigrate(&domain.User{})
+	db.AutoMigrate(&domain.Task{})
+
+	return db, nil
+}
