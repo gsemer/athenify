2ddbfdb49439205c45f5142871d86e8b208ee9d4
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-07-30 18:31:04 +0300

    server

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,60 @@
 package main
 
 import (
-	"athenify/domain"
-	"fmt"
+	"athenify/app/services"
+	"athenify/config"
+	"athenify/persistence"
+	"athenify/presentation"
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	log.Println("Loading environment variables")
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("No .env file found")
-	}
-	postgresUsername := os.Getenv("POSTGRES_USERNAME")
-	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
-	postgresDB := os.Getenv("POSTGRES_DB")
-	postgresIP := os.Getenv("POSTGRES_IP")
-	postgresPort := os.Getenv("POSTGRES_PORT")
+	postgresUsername := config.GetEnv("POSTGRES_USERNAME", "")
+	postgresPassword := config.GetEnv("POSTGRES_PASSWORD", "")
+	postgresDB := config.GetEnv("POSTGRES_DB", "")
+	postgresIP := config.GetEnv("POSTGRES_IP", "")
+	postgresPort := config.GetEnv("POSTGRES_PORT", "")
 
 	log.Println("Connecting to database")
-	source := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", postgresUsername, postgresPassword, postgresIP, postgresPort, postgresDB)
-	db, err := gorm.Open(postgres.Open(source), &gorm.Config{})
+	db, err := persistence.InitDB(postgresUsername, postgresPassword, postgresIP, postgresPort, postgresDB)
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
 	}
 
-	log.Println("Migrating")
-	db.AutoMigrate(&domain.User{})
-	db.AutoMigrate(&domain.Task{})
+	userRepository := persistence.NewUserRepository(db)
+	userService := services.NewUserService(userRepository)
+	userHandler := presentation.NewUserHandler(userService)
+
+	r := mux.NewRouter()
+	s := r.PathPrefix("/users").Subrouter()
+	s.HandleFunc("/", userHandler.Create).Methods("POST")
+	s.HandleFunc("/", userHandler.Get).Methods("GET")
+
+	server := &http.Server{
+		Addr:    "localhost:8000",
+		Handler: r,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down")
+	server.Shutdown(ctx)
+	os.Exit(0)
 }
