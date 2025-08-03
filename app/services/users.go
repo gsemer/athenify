6a62581a8351ba648013962ccc6af4bf113c4eb2
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-08-03 17:24:43 +0300

    enqueue task to result channel

--- app/services/users.go
+++ app/services/users.go
@@ -14,6 +14,18 @@ func NewUserService(ur domain.UserRepository) *UserService {
 	return &UserService{ur: ur}
 }
 
+// Create user
+type CreateUserJob struct {
+	User        domain.User
+	UserService domain.UserService
+	Result      chan domain.Result
+}
+
+func (job *CreateUserJob) Process() {
+	user, err := job.UserService.Create(job.User)
+	job.Result <- domain.Result{User: user, Error: err}
+}
+
 func (us UserService) Create(user domain.User) (domain.User, error) {
 	return us.ur.Create(user)
 }
