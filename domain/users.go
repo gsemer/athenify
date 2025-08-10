5641ba4df875a0e89a7d61afd023386ea3f732ee
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-08-10 20:33:02 +0300

    users

--- domain/users.go
+++ domain/users.go
@@ -7,12 +7,13 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
-	Username  string    `gorm:"type:text;not null;unique" json:"username"`
-	Role      string    `gorm:"type:text;not null" json:"role"`
-	Email     string    `gorm:"type:text;not null;unique" json:"email"`
-	CreatedAt time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	AuthUserID uuid.UUID `gorm:"type:uuid;not null;unique" json:"auth_user_id"`
+	Username   string    `gorm:"type:text;not null;unique" json:"username"`
+	Role       string    `gorm:"type:text;not null" json:"role"`
+	Email      string    `gorm:"type:text;not null;unique" json:"email"`
+	CreatedAt  time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP" json:"updated_at"`
 
 	Tasks []Task `gorm:"foreignKey:UserID" json:"tasks,omitempty"`
 }
